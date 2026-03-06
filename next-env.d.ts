795abf8dbf378fe5bb6ac6934d1aa035a8c99cf9
EshunB <eshunburge@gmail.com>
2026-03-06 17:26:22 -0500

    Cleanup TypeScript references in next-env.d.ts
    Removed TypeScript reference comments for Next.js types.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
+
 import "./.next/dev/types/routes.d.ts";
 
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
